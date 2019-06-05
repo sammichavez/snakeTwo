adec18b0b9b89d5293250f051553388f92cccdae
your name here <samuelchavez@students.berkeley.net>
2019-06-05 13:18:17 -0700

    c

--- Script.js
+++ Script.js
@@ -1,25 +1,58 @@
 var s;
 var scl = 20;
+
+var food;
+
 function setup() {
     createCanvas(600, 600);
     s = new Snake();
+    frameRate(10);
+    pickLocation();
+    document.getElementById("image").src = "images/download.jpeg";
+    document.getElementById("images").src = "images/download (1).jpeg";
+    document.getElementById("image1").src = "images/tenor.gif";
+
+
+}
+
+function pickLocation() {
+    var cols = floor(width/scl);
+    var rows = floor(height/scl);
+    food = createVector(floor(random(cols)), floor(random(rows)));
+    food.mult(scl);
+}
+
+function mousePressed() {
+    s.total++;
 }
 
 function draw() {
     background(51);
+
+    if (s.eat(food)) {
+        pickLocation();
+    }
+    s.death();
     s.update();
     s.show();
+
+
+    fill(255, 0, 100);
+    rect(food.x, food.y, scl, scl);
 }
+
+
+
+
 
 function keyPressed() {
     if (keyCode === UP_ARROW) {
         s.dir(0, -1);
-    }else if (keyCode === DOWN_ARROW){
+    } else if (keyCode === DOWN_ARROW) {
         s.dir(0, 1);
-    }else if (keyCode === RIGHT_ARROW){
+    } else if (keyCode === RIGHT_ARROW) {
         s.dir(1, 0);
-    }else if (keyCode === LEFT_ARROW){
+    } else if (keyCode === LEFT_ARROW) {
         s.dir(-1, 0);
     }
-
 }
